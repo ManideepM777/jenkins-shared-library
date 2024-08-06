e0eb94324d4972d5d6cc08955c3c0125b910f641
ManideepM777 <145978628+ManideepM777@users.noreply.github.com>
2024-08-06 15:55:13 +0530

    Update buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -4,18 +4,3 @@ def call() {
     echo "building the application..."
     sh 'mvn package'
 }
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t manideepm777/demo-app:jma-2.1 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push manideepm777/demo-app:jma-2.1'
-    }
-}
-
-def deployApp() {
-    echo 'deploying the application...'
-}
-
-return this
