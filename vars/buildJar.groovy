9ec1ac2fc656cbbfdcac66ab041ec6b3ffd614cb
Manideep Mavilapalli <manideepmavilapalli@Manideeps-MacBook-Pro.local>
2024-08-06 14:55:44 +0530

    initial commit

---
+++ vars/buildJar.groovy
@@ -0,0 +1,27 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo "building the application..."
+    sh 'mvn package'
+}
+
+
+
+
+
+
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t manideepm777/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push manideepm777/demo-app:jma-2.0'
+    }
+}
+
+def deployApp() {
+    echo 'deploying the application...'
+}
+
+return this
