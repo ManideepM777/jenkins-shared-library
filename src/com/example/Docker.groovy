a190495981b123d97d7c1088b9a30db40293c6d2
ManideepM777 <145978628+ManideepM777@users.noreply.github.com>
2024-08-09 14:26:02 +0530

    Update Docker.groovy

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -8,17 +8,7 @@ class Docker implements Serializable {
     Docker(script) {
         this.script = script
     }
-
-    def versionIncrement() {
-            script.echo 'incrementing app version...'
-            script.sh 'mvn build-helper:parse-version versions:set \
-                        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
-                        versions:commit'
-            def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
-            def version = matcher[0][1]
-            env.IMAGE_NAME = "$version-$BUILD_NUMBER"
-    }
-
+    
     def buildDockerImage(String imageName) {
         script.echo "building the docker image..."
         script.sh "docker build -t $imageName ."
